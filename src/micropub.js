4f58c59dd7b3607b7a9a3afc5fd39aeaf72161f3
Bret Comnes <bcomnes@gmail.com>
2018-04-17 18:25:54 -0700

    process.env

--- src/micropub.js
+++ src/micropub.js
@@ -2,6 +2,7 @@ function micropub (event, context, cb) {
   console.log(event)
   console.log(context)
   console.log(process)
+  console.log(process.env)
   cb(null, {
     statusCode: 200,
     body: 'hello world'
